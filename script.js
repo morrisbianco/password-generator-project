5a521c693c8bb447a83b92bd7068e52e98887954
Andrew Bianco <andrewbianco98@gmail.com>
2021-05-16 13:51:50 -0400

    Finished prompts and added empty array for password

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+// WHEN I click the button to generate a password *
+// THEN I am presented with a series of prompts for password criteria *
+// WHEN prompted for password criteria DONE
+// THEN I select which criteria to include in the password DONE
+// WHEN prompted for the length of the password DONE
+// THEN I choose a length of at least 8 characters and no more than 128 characters *
+// WHEN prompted for character types to include in the password DONE
+// THEN I choose lowercase, uppercase, numeric, and/or special characters DONE
+// WHEN I answer each prompt DONE
+// THEN my input should be validated and at least one character type should be selected *
+// WHEN all prompts are answered DONE
+// THEN a password is generated that matches the selected criteria *
+// WHEN the password is generated *
+// THEN the password is either displayed in an alert or written to the page *
 
 var userOption = {
   lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
@@ -21,10 +21,12 @@ var userOption = {
   specialCharacters: ["!","”","#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"],
 };
 
+var passwordAnwser = [];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var userAnwser = promptUserAnwser();
-var characterLength = (choice = 8);
+// var characterLength = (choice.value >= 8 || choice.value <= 128);
 
 // Write password to the #password input
 function writePassword() {
@@ -33,7 +35,8 @@ function writePassword() {
 
   passwordText.value = password;
 
-  
+  // var random = Math.floor(Math.random() * passwordAnwser.length);
+  // password = passwordAnwser[random];
 }
 
 function promptUserAnwser(message) {
@@ -47,29 +50,69 @@ function promptUserAnwser(message) {
 
   var choice = prompt(characterMsg);
 
+  if (choice >= 8 && choice <= 128) {
+    // Insert Password Length code
+    alert("Your password will be " + choice +" characters long.");
+  } 
+  // else {
+  //       var invalid = "You did not choose a length between 8 characters and 128 characters, please choose again."; 
+  //       promptUserAnwser(invalid);
+  //   }
+
   console.log(choice);
 
-  if (characterLength) {
+  // if (choice.value >= 8 || choice.value <= 128) {
+  //   return choice;
+  // } else {
+  //     // var invalid = "You did not choose a length between 8 characters and 128 characters, please choose again."; 
+  //     // promptUserAnwser(invalid);
+  // }
+
     var lowercase = confirm("Would you like to include lowercase letters?");
     console.log(lowercase);
-  } else  
+    if (lowercase === true) {
+      passwordAnwser.push(userOption.lowercase);
+      alert("Lowercase letters will be added to your password.");
+    } else {
+      alert("Lowercase letters will not be added to your password.");
+    }
 
-  if ( ) {
     var uppercase = confirm("Would you like to include uppercase letters?");
     console.log(uppercase);
-  } else 
-
-  if ( ) {
+    if (uppercase === true) {
+      passwordAnwser.push(userOption.uppercase);
+      alert("Uppercase letters will be added to your password.");
+    } else {
+      alert("Uppercase letters will not be added to your password.");
+    }
+  
     var numbers = confirm("Would you like to include numbers?");
     console.log(numbers);
-  } else 
-
-  if ( ) {
+    if (numbers === true) {
+      passwordAnwser.push(userOption.numbers);
+      alert("Numbers will be added to your password.");
+    } else {
+      alert("Numbers will not be added to your password.");
+    }
+  
     var specialCharacters = confirm("Would you like to include special characters?");
     console.log(specialCharacters);
-  } else 
-}
+    if (specialCharacters === true) {
+      passwordAnwser.push(userOption.specialCharacters);
+      alert("Special characters will be added to your password.");
+    } else {
+      alert("Special characters will not be added to your password.");
+    }
+
+    console.log(passwordAnwser);
+  }
+//     if (lowercase, uppercase, numbers, specialCharacters === false) {
+//       alert("You must choose at least one criteria.")
+//       promptUserAnwser();
+//     } else {
+//       console.log(passwordAnwser);
+//     }
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-promptUserAnwser();
